399fcf419b8f9f5ac73268a5354454b2794e1b46
N0042171 <N0042171@AMIN-1803-0050.goldmantis.com>
2018-11-02 16:01:39 +0800

    1

---
+++ ucanuupblog/src/main/java/ucanuup/cc/config/db/jpa/UserIDAuditorBean.java
@@ -0,0 +1,24 @@
+package ucanuup.cc.config.db.jpa;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+
+/**
+ * Description: TODO[描述该类概要功能介绍]
+ *
+ * @author guwenbin
+ * @date 2017年11月24日
+ * @version 1.0
+ */
+@Configuration
+public class UserIDAuditorBean implements AuditorAware<String>{
+
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		
+		return null;
+	}
+}
