6a92a250054c3741429d6bc37f7677d7a86a04a5
N0042171 <N0042171@AMIN-1803-0050.goldmantis.com>
2018-11-07 12:28:40 +0800

    exception

---
+++ ucanuupblog/src/main/java/ucanuup/cc/common/exceptions/SystemException.java
@@ -0,0 +1,14 @@
+package ucanuup.cc.common.exceptions;
+
+public class SystemException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public SystemException() {
+		super();
+	}
+	
+	public SystemException(String msg) {
+		super(msg);
+	}
+}
---
+++ ucanuupblog/src/main/java/ucanuup/cc/common/exceptions/ValidateException.java
@@ -0,0 +1,14 @@
+package ucanuup.cc.common.exceptions;
+
+public class ValidateException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public ValidateException() {
+		super();
+	}
+	
+	public ValidateException(String msg) {
+		super(msg);
+	}
+}
