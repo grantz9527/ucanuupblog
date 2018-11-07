4e81a00c2ed36957a8be26d827571903fdbf5efa
N0042171 <N0042171@AMIN-1803-0050.goldmantis.com>
2018-11-07 15:41:04 +0800

    1

--- ucanuupblog/src/main/java/ucanuup/cc/blog/web/bbs/controller/BbsController.java
+++ ucanuupblog/src/main/java/ucanuup/cc/blog/web/bbs/controller/BbsController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import ucanuup.cc.blog.web.bbs.model.LeavingMsgModel;
 import ucanuup.cc.blog.web.core.model.RegisterModel;
+import ucanuup.cc.common.web.query.BaseQueryModel;
 import ucanuup.cc.common.web.rt.RtMsg;
 import ucanuup.cc.common.web.rt.RtType;
 
@@ -24,14 +25,21 @@ public class BbsController {
 	
 	@GetMapping("list")
 	@ApiOperation(value = "查询留言板分页信息", notes = "查询留言板分页信息")
-	public RtMsg<String> list(@RequestBody @ApiParam(name="请求修改密码对象",value="传入json格式",required=true) RegisterModel model){
+	public RtMsg<String> list(@RequestBody @ApiParam(name="分页查询条件",value="传入json格式") BaseQueryModel model){
 		
 		return new RtMsg<String>(RtType.VALID,"sdfjslkdfj");
 	}
 	
 	@PostMapping("addLeavingMsg")
-	@ApiOperation(value = "添加留言", notes = "添加留言评论")
+	@ApiOperation(value = "添加留言", notes = "添加留言评论,第一级别的评论 ,直接评论!")
 	public RtMsg<String> addLeavingMsg(@RequestBody @ApiParam(name="请求修改密码对象",value="传入json格式",required=true) LeavingMsgModel model){
+		
+		return new RtMsg<String>(RtType.VALID,"sdfjslkdfj");
+	}
+	
+	@PostMapping("addLeavingMsgSun")
+	@ApiOperation(value = "添加留言子评论", notes = "添加留言子评论,在留言下添加评论信息")
+	public RtMsg<String> addLeavingMsgSun(@RequestBody @ApiParam(name="请求修改密码对象",value="传入json格式",required=true) LeavingMsgModel model){
 		
 		return new RtMsg<String>(RtType.VALID,"sdfjslkdfj");
 	}
