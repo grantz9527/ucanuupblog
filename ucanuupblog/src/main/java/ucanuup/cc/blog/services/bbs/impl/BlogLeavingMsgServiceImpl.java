fadee9414ad410f49bc3312a7b7c5a897944898c
N0042171 <N0042171@AMIN-1803-0050.goldmantis.com>
2018-11-07 11:59:34 +0800

    1

--- ucanuupblog/src/main/java/ucanuup/cc/blog/services/bbs/impl/BlogLeavingMsgServiceImpl.java
+++ ucanuupblog/src/main/java/ucanuup/cc/blog/services/bbs/impl/BlogLeavingMsgServiceImpl.java
@@ -2,9 +2,23 @@ package ucanuup.cc.blog.services.bbs.impl;
 
 import org.springframework.stereotype.Service;
 
+import ucanuup.cc.blog.services.bbs.entity.BlogLeavingMsg;
+import ucanuup.cc.blog.services.bbs.entity.BlogLeavingMsgSun;
 import ucanuup.cc.blog.services.bbs.inter.BlogLeavingMsgService;
 
 @Service
 public class BlogLeavingMsgServiceImpl implements BlogLeavingMsgService{
+
+	@Override
+	public BlogLeavingMsg saveBlogLeavingMsg(BlogLeavingMsg entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BlogLeavingMsgSun saveBlogLeavingMsgSun(BlogLeavingMsgSun entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- ucanuupblog/src/main/java/ucanuup/cc/blog/services/bbs/inter/BlogLeavingMsgService.java
+++ ucanuupblog/src/main/java/ucanuup/cc/blog/services/bbs/inter/BlogLeavingMsgService.java
@@ -2,7 +2,13 @@ package ucanuup.cc.blog.services.bbs.inter;
 
 import org.springframework.stereotype.Service;
 
+import ucanuup.cc.blog.services.bbs.entity.BlogLeavingMsg;
+import ucanuup.cc.blog.services.bbs.entity.BlogLeavingMsgSun;
+
 @Service
 public interface BlogLeavingMsgService {
 
+	BlogLeavingMsg saveBlogLeavingMsg(BlogLeavingMsg entity);
+	
+	BlogLeavingMsgSun saveBlogLeavingMsgSun(BlogLeavingMsgSun entity);
 }
