c5be960ff3851b8875e81612a0aa1f360d1986c1
N0042171 <N0042171@AMIN-1803-0050.goldmantis.com>
2018-11-07 12:51:02 +0800

    1

---
+++ ucanuupblog/src/main/java/ucanuup/cc/common/utils/AppUtil.java
@@ -0,0 +1,12 @@
+package ucanuup.cc.common.utils;
+
+import ucanuup.cc.common.utils.supportobj.UserInfo;
+
+public final class AppUtil {
+
+	
+	public static final UserInfo getUser() {
+		
+		return null;
+	}
+}
---
+++ ucanuupblog/src/main/java/ucanuup/cc/common/utils/supportobj/UserInfo.java
@@ -0,0 +1,55 @@
+package ucanuup.cc.common.utils.supportobj;
+
+public class UserInfo {
+	
+	private String id;
+	
+	private String account;
+	
+	private String nickName;
+	
+	private String email;
+	
+	private String phone;
+	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
